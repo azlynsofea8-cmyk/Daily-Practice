82e5cf4f26de38bcf6e78141826c1d70b36d989a
azlynsofea8-cmyk <azlynsofea8@gmail.com>
2025-12-02 16:34:51 +0800

    Add files via upload

---
+++ Day 07_021225/mypkg/newfun/funthings.py
@@ -0,0 +1,12 @@
+def salam(ns):
+    return f"Selamat {ns}!!!"
+
+# create one class 
+# one attribte save name
+# one method welcome(name): return f"Welcome {name}!!!"
+
+class SelamatDatang:
+    name = "Muhammad Ali"
+
+    def welcome(name):
+        return f"Welcome: {name}!!"
